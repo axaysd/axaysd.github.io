1c17c397340243d7293b9e32f910fa60276fecad
Akshay S Danthi <axaysd@gmail.com>
2023-12-04 16:22:53 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('start-button').addEventListener('click', function() {
+  alert('Starting the simulation!');
+  // Here you can add more JavaScript to handle what happens when the simulation starts
+});
