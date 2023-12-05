ebb9be87732a85799c0bd529819dc30fa2c9b294
Akshay S Danthi <axaysd@gmail.com>
2023-12-05 01:07:33 -0800

    negligible

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('start-button').addEventListener('click', function() {
 async function fetchQuestion() {
   try {
     const response = await fetch('https://g8sk9p34ve.execute-api.us-west-1.amazonaws.com/prod');
-    if (!response.ok) throw new Error('Network response was not ok.');
+    if (!response.ok) throw new Error('Network response was not ok!');
 
     const data = await response.json();
     document.getElementById('question-box').textContent = data.question;
