299ca7d188ff31e526acb41b9007d2da2a7777ab
Akshay S Danthi <axaysd@gmail.com>
2023-12-05 01:05:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ document.getElementById('start-button').addEventListener('click', function() {
   alert('Starting the simulation!');
   // Here you can add more JavaScript to handle what happens when the simulation starts
 });
+
+async function fetchQuestion() {
+  try {
+    const response = await fetch('https://g8sk9p34ve.execute-api.us-west-1.amazonaws.com/prod');
+    if (!response.ok) throw new Error('Network response was not ok.');
+
+    const data = await response.json();
+    document.getElementById('question-box').textContent = data.question;
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
+
+fetchQuestion();
